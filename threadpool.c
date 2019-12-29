48d1ecefa62d355758e59f8461a107d336426fff
cezarybednarz <cezary.bednarz@gmail.com>
2019-12-29 23:59:12 +0100

    starting to write threadpool

--- threadpool.c
+++ threadpool.c
@@ -1,7 +1,67 @@
 #include "threadpool.h"
 
-int thread_pool_init(thread_pool_t *pool, size_t num_threads) { return 0; }
 
-void thread_pool_destroy(struct thread_pool *pool) {}
+// JAK NA RAZIE DLA CZYTELNOSCI POMINE OBSLUGIWANIE BLEDOW
 
-int defer(struct thread_pool *pool, runnable_t runnable) { return 0; }
+/* ---- error handling ---- */
+void syserr(int bl, const char *fmt, ...)
+{
+  va_list fmt_args;
+
+  fprintf(stderr, "ERROR: ");
+
+  va_start(fmt_args, fmt);
+  vfprintf(stderr, fmt, fmt_args);
+  va_end (fmt_args);
+  fprintf(stderr," (%d; %s)\n", bl, strerror(bl));
+  exit(1);
+}
+
+/* ---- binary semaphore ---- */
+static void semaphore_post(binary_semaphore_t *sem) {
+  pthread_mutex_lock(&sem->mutex);
+  if(sem->v == 1)
+    syserr(1, "sem->v == 1");
+  sem->v += 1;
+  pthread_cond_signal(&sem->condition);
+  pthread_mutex_unlock(&sem->mutex);
+}
+
+static void semaphore_wait(binary_semaphore_t *sem) {
+  pthread_mutex_lock(&sem->mutex);
+  while (!sem->v)
+    pthread_cond_wait(&sem->condition, &sem->mutex);
+  sem->v -= 1;
+  pthread_mutex_unlock(&sem->mutex);
+}
+
+/* ---- defer queue ---- */
+
+static int defer_queue_init(defer_queue_t *q) {
+
+}
+
+static int defer_queue_push(defer_queue_t *q) {
+
+}
+
+static runnable_t *defer_queue_pop(defer_queue_t *q) {
+
+}
+
+static int defer_queue_destroy(defer_queue_t *q) {
+
+}
+
+/* ---- thread pool ---- */
+int thread_pool_init(thread_pool_t *pool, size_t num_threads) {
+  return 0;
+}
+
+void thread_pool_destroy(struct thread_pool *pool) {
+
+}
+
+int defer(struct thread_pool *pool, runnable_t runnable) {
+  return 0;
+}
--- threadpool.h
+++ threadpool.h
@@ -1,7 +1,15 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
+#include <pthread.h>
+#include <semaphore.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdarg.h>
+#include <string.h>
 
 typedef struct runnable {
   void (*function)(void *, size_t);
@@ -9,7 +17,35 @@ typedef struct runnable {
   size_t argsz;
 } runnable_t;
 
+typedef struct binary_semaphore {
+  pthread_mutex_t mutex;
+  pthread_cond_t condition;
+  int v;
+} binary_semaphore_t;
+
+
+typedef struct node {
+  runnable_t *runnable;
+  node *prev;
+} node_t;
+
+typedef struct defer_queue {
+
+  node_t *front;
+  node_t *back;
+  int length;
+
+} defer_queue_t;
+
 typedef struct thread_pool {
+
+  size_t num_threads;
+  pthread_t **threads;
+
+  size_t num_free_threads;
+
+  defer_queue_t defer_queue;
+
 
 } thread_pool_t;
 
